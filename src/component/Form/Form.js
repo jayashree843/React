d69a451408e6ef2bffe44e057bda5fde0dbcc4e5
jayashree843 <jayashreep058@gmail.com>
2024-05-02 17:20:10 +0530

    Updated component

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,108 +1,106 @@
+import { useState, useEffect } from 'react';
 import '../Form/Form.css';
-import { useState,useEffect } from 'react';
 import axios from 'axios';
+import img from '../../Images/del.png';
 export default function Form() {
-    const initialValues={username: "",email: "", phonenumber: "", query: ""}
-    const [formValues, setFormValues]=useState(initialValues);
-    const [formErrors, setFormErrors]=useState({});
-    const [isSubmit, setIsSubmit]=useState(false);
+    const initialValues = { username: "", email: "", phonenumber: "", query: "" }
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
 
-    const handleChange=(e)=>{
-      
-        const {name, value}=e.target;
-        setFormValues({...formValues, [name]:value});
+    const handleChange = (e) => {
+
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
         console.log(formValues);
     };
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
         setFormErrors(validate(formValues));
         setIsSubmit(true);
-        const username=e.target.username.value;
-        const email=e.target.email.value;
-        const phonenumber=e.target.phonenumber.value;
-        const query=e.target.query.value;
-        axios.post("http://admin.turtu.in/query",{
+        const username = e.target.username.value;
+        const email = e.target.email.value;
+        const phonenumber = e.target.phonenumber.value;
+        const query = e.target.query.value;
+        axios.post("http://admin.turtu.in/query", {
             username,
             email,
             phonenumber,
             query
         })
-        .then((response)=>{
-            console.log(response);
-            e.target.reset();
-        })
-        .catch((error)=>{
-            console.log(error);
-        });
-   
+            .then((response) => {
+                console.log(response);
+                e.target.reset();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
     };
-    useEffect(()=>{
+    useEffect(() => {
         console.log(formErrors)
-        if(Object.keys(formErrors).length===0 && isSubmit){
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
             console.log(formValues);
         }
     })
-    const validate=(values)=>{
-        const errors={}
-        const regex="_%+-]+@[a-zA-Z0-9. -]+\\. [a-zA-Z]{2,}$/";
-        if(!values.username){
-            errors.username="username is required";
+    const validate = (values) => {
+        const errors = {}
+        const regex = "_%+-]+@[a-zA-Z0-9. -]+\\. [a-zA-Z]{2,}$/";
+        if (!values.username) {
+            errors.username = "username is required";
         }
-        if(values.username<2){
-            errors.username="username must be more that 2 characters";
+        if (values.username < 2) {
+            errors.username = "username must be more that 2 characters";
         }
-        if(!values.email){
-            errors.email="email is required";
-        } 
-        
-        if(!values.phonenumber){
-            errors.phonenumber="phone number is required";
+        if (!values.email) {
+            errors.email = "email is required";
         }
-        else if(values.phonenumber<10){
-            errors.phonenumber="phone number is not valid";
+
+        if (!values.phonenumber) {
+            errors.phonenumber = "phone number is required";
         }
-        if(!values.query){
-            errors.query="query message is required";
+        else if (values.phonenumber < 10) {
+            errors.phonenumber = "phone number is not valid";
+        }
+        if (!values.query) {
+            errors.query = "query message is required";
         }
         return errors;
 
     }
     return (
-        <section className="container my-5">
+        <><section className="container my-5">
             <div>
                 <div className="heading text-center">
                     <h3>Contact Us</h3>
                     <p>Leave us a message</p>
                 </div>
                 <div className="form py-4">
-                
+
                     <form onSubmit={handleSubmit}>
-                        
+
                         <div className="input-control">
                             <label for="fname">User Name</label>
                             <input type="text" name='username' placeholder="Username" value={formValues.username}
-                            onChange={handleChange}
-                            />
+                                onChange={handleChange} />
                         </div>
                         <p>{formErrors.username}</p>
                         <div className="input-control">
                             <label for="fname">Email Address</label>
                             <input type="email" name='email' placeholder="Email Address" value={formValues.email}
-                            onChange={handleChange}
-                            />
+                                onChange={handleChange} />
                         </div>
                         <p>{formErrors.email}</p>
                         <div className="input-control">
                             <label for="fname">Phone Number</label>
-                            <input type="text" name='phonenumber' placeholder="Phone Number" value={formValues.phonenumber} 
-                            onChange={handleChange}
-                            />
+                            <input type="text" name='phonenumber' placeholder="Phone Number" value={formValues.phonenumber}
+                                onChange={handleChange} />
                         </div>
                         <p>{formErrors.phonenumber}</p>
                         <div className="input-control">
                             <label for="subject">Query</label>
-                            <textarea id="Query" name='query' placeholder="Write something.." value={formValues.query} 
-                            onChange={handleChange}
+                            <textarea id="Query" name='query' placeholder="Write something.." value={formValues.query}
+                                onChange={handleChange}
                             ></textarea>
                         </div>
                         <p>{formErrors.query}</p>
@@ -110,22 +108,23 @@ export default function Form() {
                             <button className=''>Submit</button>
                         </div>
 
-                        {Object.keys(formErrors).length===0 && isSubmit ? (<div className='message-success'>Message sent successfully !!</div>
-                 ):(
-                     <pre>{JSON.stringify(formValues, undefined,2)}</pre> 
-                     )}
-                    
+                        {Object.keys(formErrors).length === 0 && isSubmit ? (<div className='message-success'>Message sent successfully !!</div>
+                        ) : (
+                            <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+                        )}
 
 
 
-                        
+
+
                     </form>
                 </div>
-
+               
 
             </div>
 
         </section>
+            </>
 
     );
 } 
